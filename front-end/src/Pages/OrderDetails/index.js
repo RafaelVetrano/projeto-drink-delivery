efa9852f98bde30b4216c61c3c612e614354760d
Koramini <lucas.faleirosilva@gmail.com>
2022-12-12 20:29:03 -0300

    feat: Adiciona o componente OrderDetaisInfo e adiciona estrutura da OrderDetails

--- front-end/src/Pages/OrderDetails/index.js
+++ front-end/src/Pages/OrderDetails/index.js
@@ -1,46 +1,44 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 // import { useNavigate } from 'react-router-dom';
 import Header from '../../Components/Header';
-import OrderSale from '../../Components/OrderSale';
+// import OrderSale from '../../Components/OrderSale';
 import TableHeader from '../../Components/TableHeader';
-import AppContext from '../../Context/AppContext';
-import TotalPriceButton from '../../Components/TotalPriceButton';
+// import AppContext from '../../Context/AppContext';
+import OrderDetailsInfo from '../../Components/OrderDetailsInfo';
 // const navigate = useNavigate();
 
 function OrderDetails() {
-  const { products, setProducts, totalPrice, setTotalPrice } = useContext(AppContext);
+  const [sale, setSale] = useState([]);
+  const { id } = useParams();
 
   useEffect(() => {
-    const cart = JSON.parse(localStorage.getItem('carrinho'));
-    setProducts(cart);
-    setTotalPrice(
-      cart.reduce((acc, sale) => acc + (Number(sale.price) * sale.quantity), 0),
-    );
-  }, [setProducts, setTotalPrice]);
+    console.log(id);
+    const request = async () => {
+      const response = await fetch(`http://localhost:3001/customer/orders/${id}`);
+      const data = await response.json();
+      setSale(data);
+    };
+    request();
+  }, [id]);
 
   return (
     <div>
       <Header />
       <div>
         <h2>Detalhes do Pedido</h2>
+        <OrderDetailsInfo
+          key={ sale.id }
+          orderId={ sale.id }
+          sellerName={ sale.sellerName } // Só exemplo, vai ter que mudar depois, eu acho
+          totalPrice={ sale.totalPrice }
+          data={ sale.saleDate }
+          status={ sale.status }
+        />
         <table>
           <TableHeader />
-          <tbody>
-            {products.map((sale, index) => (
-              <OrderSale
-                key={ `${sale.name}${index}` }
-                index={ index }
-                name={ sale.name }
-                price={ sale.price }
-                quantity={ sale.quantity }
-              />
-            ))}
-          </tbody>
+          <tbody />
         </table>
-        <TotalPriceButton
-          totalPrice={ totalPrice }
-          testId="customer_order_details__element-order-total-price"
-        />
       </div>
     </div>
   );
